b32111a02de19bbd6b5e024526b85866a73ae3ed
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2021-05-17 01:33:15 +0200

    Aufgab 4, binary search

--- src/suchen/BinarySearch1.java
+++ src/suchen/BinarySearch1.java
@@ -1,40 +1,45 @@
 package suchen;
 
 /**
- * Boolean binary search
+ * binary search
  */
 public class BinarySearch1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Test");
+        System.out.println("Log before");
         
+        byte[] a = new byte[] {10};
+
+        for (int i=0; i<a.length; i++) {
+            a[i] = (byte) (i);
+        }
         
+        byte value = 7;
+        int result = binarySearch(a, value);
+
+        System.out.println(result);
+
+        System.out.println("Log after");
 
     }
     
-    public static boolean binarySearch(byte[] a, byte value) {
+    public static int binarySearch(byte[] a, byte value) {
 
-        int first = -1;
-        int last = a.length;
+        int first = 0;
+        int last = a.length-1;
 
         while (first <= last) {
             int m = (first + last) >>> 1; 
             if (a[m] < value) {
-            first = m + 1;
+                first = m + 1;
             } else if (a[m] == value) {
-            return true;
+                return m;
             } else {
             last = m - 1;
             }
         }
-
-
-
-
-
-
-        return false;
+        return -1;
     }
 
 }
